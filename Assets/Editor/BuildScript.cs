be6c310d1047c5d028f8f9b08facc0e235ccca12
Johnathon Slightham <slighthamj@gmail.com>
2026-01-08 23:48:09 -0500

    Linux build command

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,14 +1,39 @@
 using UnityEditor;
+using System.IO;
 
 public class BuildScript
 {
     public static void BuildWindows()
     {
+        string outputPath = "Builds/Windows/botchain.exe";
+
         BuildPipeline.BuildPlayer(
             new[] { "Assets/Scenes/SampleScene.unity" },
-            "Builds/Windows/botchain.exe",
+            outputPath,
             BuildTarget.StandaloneWindows64,
             BuildOptions.None
         );
+
+        string sourceSoPath = "Assets/ControlLibrary/libc_control.dll";
+        string destSoPath = Path.Combine(Path.GetDirectoryName(outputPath), "libc_control.dll");
+
+        File.Copy(sourceSoPath, destSoPath, overwrite: true);
+    }
+
+    public static void BuildLinux()
+    {
+        string outputPath = "Builds/Linux/botchain.x86_64";
+
+        BuildPipeline.BuildPlayer(
+            new[] { "Assets/Scenes/SampleScene.unity" },
+            outputPath,
+            BuildTarget.StandaloneLinux64,
+            BuildOptions.None
+        );
+
+        string sourceSoPath = "Assets/ControlLibrary/libc_control.so";
+        string destSoPath = Path.Combine(Path.GetDirectoryName(outputPath), "libc_control.so");
+
+        File.Copy(sourceSoPath, destSoPath, overwrite: true);
     }
 }
